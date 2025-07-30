0dde67b4e9922d39430e9bff07c4b00d69b90411
Sean Molenaar <sean.molenaar@framna.com>
2025-07-30 14:03:04 +0200

    chore: cleanup client init

--- reviewboard/main.py
+++ reviewboard/main.py
@@ -15,11 +15,11 @@ def get_review_data(commit_body):
     pattern = re.compile(r"https://([a-z\.\-]*).*?/r/([0-9]*)")
 
     if rb_match := rb_pattern.search(commit_body):
-        print(f'::error::RBCommons match found: url={rb_match.group(1)}, id={rb_match.group(2)}')
-        return {'domain': 'rbcommons.com', 'team': rb_match.group(1), 'review_id': rb_match.group(2)}
+        print(f'::info::RBCommons match found: team={rb_match.group(1)}, id={rb_match.group(2)}')
+        return {'domain': 'rbcommons.com', 'team': f'/s/{rb_match.group(1)}', 'review_id': rb_match.group(2)}
     elif match := pattern.search(commit_body):
-        print(f'::error::Match found: url={match.group(1)}, id={match.group(2)}')
-        return {'domain': match.group(1), 'review_id': match.group(2)}
+        print(f'::info::Match found: url={match.group(1)}, id={match.group(2)}')
+        return {'domain': match.group(1), 'team': '', 'review_id': match.group(2)}
     else:
         return None
 
@@ -27,16 +27,12 @@ def get_review_data(commit_body):
 def get_reviewboard_client():
     commit_body = subprocess.check_output(['git', 'show', '--pretty=format:%b', '-s'], cwd=cwd).decode().strip()
     review_data = get_review_data(commit_body)
-    url = None
     if not review_data:
         print('::error::No review data found')
         exit(1)
-    elif not review_data.get('team'):
-        url = f'https://{review_data.get("domain")}'
-    else:
-        url = f'https://rbcommons.com/s/{review_data.get("team")}'
 
-    return RBClient(url, api_token=reviewboard_token)
+    return RBClient(f'https://{review_data.get("domain")}{review_data.get("team")}',
+                    api_token=reviewboard_token.strip())
 
 
 root = get_reviewboard_client().get_root()
