e31780d58c860a3888fd890585f7d77ea80903fd
Sean Molenaar <sean.molenaar@framna.com>
2025-07-30 10:13:20 +0200

    chore: cleanup python style

--- database-migration/main.py
+++ database-migration/main.py
@@ -1,6 +1,6 @@
-#!/usr/bin/env python3.13
+#!/usr/bin/env python3
 
-from os import listdir,environ,remove
+from os import listdir, environ
 from os.path import isfile, isdir, join
 from packaging.version import Version
 import subprocess
@@ -13,14 +13,25 @@ mysql_port = environ.get('MYSQL_PORT')
 mysql_user = environ.get('MYSQL_USER')
 mysql_password = environ.get('MYSQL_PASSWORD')
 
-default_args = [f"--user={mysql_user}", f"--password={mysql_password}", f"--host={mysql_host}", f"--port={mysql_port}"]
-dump_default_args = default_args + ["--skip-comments", "--compact", "--disable-keys", "--no-create-db"]
+default_args = [
+    f"--user={mysql_user}",
+    f"--password={mysql_password}",
+    f"--host={mysql_host}",
+    f"--port={mysql_port}"
+]
+dump_default_args = default_args + [
+    "--skip-comments",
+    "--compact",
+    "--disable-keys",
+    "--no-create-db"
+]
 
 db_prefix = environ.get('DB_PREFIX', str(uuid.uuid4()).split("-")[0])
 schema_db_name = f"{db_prefix}_schema"
 update_db_name = f"{db_prefix}_update"
 
 current_schema_file = "docs/database/schema.sql"
+
 
 def command(cmd, file="", input=None):
     line = " ".join(cmd)
@@ -29,22 +40,31 @@ def command(cmd, file="", input=None):
     if dry_run:
         return ""
     if input:
-        result = subprocess.run(cmd, capture_output=True, text=True, stderr=subprocess.STDOUT, input=input)
+        result = subprocess.run(cmd,
+                                capture_output=True,
+                                text=True,
+                                stderr=subprocess.STDOUT,
+                                input=input)
     else:
-        result = subprocess.run(cmd, capture_output=True, text=True, stderr=subprocess.STDOUT)
+        result = subprocess.run(cmd,
+                                capture_output=True,
+                                text=True,
+                                stderr=subprocess.STDOUT)
     try:
         result.check_returncode()
-    except CalledProcessError as e:
+    except subprocess.CalledProcessError as e:
         out = e.stdout
         print(f"::error{file_info}::Command failed: \n{out}")
         exit(1)
     return result.stdout
+
 
 def file_content(filename):
     file = open(filename, 'r')
     output = file.read()
     file.close()
     return output
+
 
 def cmd_to_file(cmd, filename):
     output = command(cmd).replace(schema_db_name, 'DATABASE').replace(update_db_name, 'DATABASE')
@@ -53,10 +73,15 @@ def cmd_to_file(cmd, filename):
     file.write(output)
     file.close()
 
+
 def import_schema():
     command(['mysql'] + default_args + ['-e', f"CREATE DATABASE IF NOT EXISTS {schema_db_name};"])
-    command(['mysql'] + default_args + [schema_db_name], input=file_content(current_schema_file), file=current_schema_file)
-    cmd_to_file((['mysqldump', '--databases', schema_db_name] + dump_default_args), f'{schema_db_name}.dump.sql')
+    command(['mysql'] + default_args + [schema_db_name],
+            input=file_content(current_schema_file),
+            file=current_schema_file)
+    cmd_to_file((['mysqldump', '--databases', schema_db_name] + dump_default_args),
+                file=f'{schema_db_name}.dump.sql')
+
 
 def import_updates():
     command(['mysql', '-e', f'CREATE DATABASE IF NOT EXISTS {update_db_name};'] + default_args)
@@ -64,20 +89,21 @@ def import_updates():
     print(f"::debug::Running in: {working_dir}")
     tag_list_str = command(['git', 'tag', '--list'])
     print(f"::debug::Tag list: {tag_list_str}")
-    
+
     tag_list = tag_list_str.partition('\n')
     if not tag_list_str:
-        print(f"::error::Empty tag list, please check the git clone!")
+        print("::error::Empty tag list, please check the git clone!")
         exit(1)
     elif not tag_list:
-        print(f"::error::Tag list not formatted correctly!")
+        print("::error::Tag list not formatted correctly!")
         exit(1)
 
     first_tag = tag_list[0]
     if not first_tag:
-        print(f"::error::Could not find first tag, please check the git clone!")
+        print("::error::Could not find first tag, please check the git clone!")
         exit(1)
-    command(['mysql'] + default_args + [update_db_name], input=command(['git', 'show', f"{first_tag}:{current_schema_file}"]))
+    command(['mysql'] + default_args + [update_db_name], 
+            input=command(['git', 'show', f"{first_tag}:{current_schema_file}"]))
 
     p = re.compile(r"update_([0-9\.]*)(?:_to)?_[0-9\.]*\.sql")
     files = {}
@@ -96,9 +122,13 @@ def import_updates():
     versions.sort(key=Version)
     for version in versions:
         file = f"{updates_path}/{files[version]}"
-        command(['mysql'] + default_args + [update_db_name], input=file_content(file), file=file)
+        command(['mysql'] + default_args + [update_db_name],
+                input=file_content(file),
+                file=file)
 
-    cmd_to_file((['mysqldump', '--databases', update_db_name] + dump_default_args), f'{update_db_name}.dump.sql')
+    cmd_to_file((['mysqldump', '--databases', update_db_name] + dump_default_args),
+                file=f'{update_db_name}.dump.sql')
+
 
 if not isfile(current_schema_file):
     print("::error::No current schema!")
@@ -113,4 +143,9 @@ if not isdir(updates_path):
 
 import_updates()
 
-command(['diff', '--side-by-side', '--suppress-common-lines', '-W', '200', f'{update_db_name}.dump.sql', f'{schema_db_name}.dump.sql'])
+command([
+    'diff', '--side-by-side',
+            '--suppress-common-lines',
+            '-W', '200',
+            f'{update_db_name}.dump.sql', f'{schema_db_name}.dump.sql'
+])
